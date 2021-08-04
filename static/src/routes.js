5adefff90182b5148beb5e3e3050fb092ddd49a0
IT.SOS <peng.yu@qjfu.cn>
2021-08-04 08:06:40 +0800

    add tiptap.

--- static/src/routes.js
+++ static/src/routes.js
@@ -2,11 +2,14 @@ import {createRouter, createWebHistory} from "vue-router";
 
 const Home = () => import(/* webpackChunkName: "group-front" */ './components/Home.vue')
 const Article = () => import(/* webpackChunkName: "group-front" */ './components/Article.vue')
+const Editor = () => import(/* webpackChunkName: "group-front" */ './components/Editor.vue')
 
 const routes = [
     { path: '/', component: Home },
     { path: '/:keyword', component: Home },
     { path: '/a/:title', component: Article },
+    { path: '/e/', component: Editor },
+    { path: '/e/:title', component: Editor },
 ]
 
 export const router = createRouter({
--- static/vite.config.js
+++ static/vite.config.js
@@ -2,5 +2,10 @@ module.exports = {
     runtimeCompiler: true,
     alias: {
         'vue': "vue/dist/vue.esm-bundler.js"
+    },
+    cssPreprocessOptions: {
+        scss: {
+            // additionalData: '@import "./src/assets/scss/style.scss";' // 添加公共样式
+        }
     }
 }
