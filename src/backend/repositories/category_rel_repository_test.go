d2e260643d6890af7e43ebb66947b26f0a427300
IT.SOS <peng.yu@qjfu.cn>
2021-08-29 19:13:03 +0800

    【新增】 新增测试用例

--- src/backend/repositories/category_rel_repository_test.go
+++ src/backend/repositories/category_rel_repository_test.go
@@ -9,11 +9,11 @@ import (
 func Test_categoryRelRepository_Delete(t *testing.T) {
 	var (
 		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	id := RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
+	id := RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
 	actual := RCategoryRel.Delete(id)
 	if actual != expected {
 		t.Errorf("RCategoryRel.Delete(%d) = %t; expected %t", id, actual, expected)
@@ -23,109 +23,98 @@ func Test_categoryRelRepository_Delete(t *testing.T) {
 func Test_categoryRelRepository_DeleteByAid(t *testing.T) {
 	var (
 		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAid(in2)
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.DeleteByAid(aid)
 	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAid(%d) = %t; expected %t", in2, actual, expected)
+		t.Errorf("RCategoryRel.DeleteByAid(%d) = %t; expected %t", aid, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_DeleteByAidAndCid(t *testing.T) {
 	var (
 		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAidAndCid(in1, in2)
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.DeleteByAidAndCid(aid, cid)
 	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAidAndCid(%d, %d) = %t; expected %t", in1, in2, actual, expected)
+		t.Errorf("RCategoryRel.DeleteByAidAndCid(cid=%d, aid=%d) = %t; expected %t", aid, cid, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_DeleteByCid(t *testing.T) {
 	var (
 		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAidAndCid(in1, in2)
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.DeleteByAidAndCid(aid, cid)
 	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAidAndCid(%d, %d) = %t; expected %t", in1, in2, actual, expected)
+		t.Errorf("RCategoryRel.DeleteByAidAndCid(aid=%d, cid=%d) = %t; expected %t", aid, cid, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_GetCountByCid(t *testing.T) {
 	var (
 		expected = uint(1)
-		in1      = uint(1)
-		in2      = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.GetCountByCid(in1)
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.GetCountByCid(cid)
 	if actual != expected {
-		t.Errorf("RCategoryRel.GetCountByCid(%d) = %d; expected %d", in1, actual, expected)
+		t.Errorf("RCategoryRel.GetCountByCid(%d) = %d; expected %d", cid, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_Select(t *testing.T) {
 	var (
-		expected = uint(1)
-		in1      = uint(1)
-		in2      = uint(1)
+		expected = uint(2)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
 	mysql.NewMysql().Exec("truncate table category_rel")
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual, _ := RCategoryRel.Select(&datamodels.CategoryRel{Cid: in1, Aid: in2})
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual, _ := RCategoryRel.Select(&datamodels.CategoryRel{Cid: cid, Aid: aid})
 
 	if actual.Aid != expected {
-		t.Errorf("RCategoryRel.Select(%v) = %v; expected aid=%v", &datamodels.CategoryRel{Cid: in1, Aid: in2}, actual, expected)
+		t.Errorf("RCategoryRel.Select(%v) = %v; expected aid=%v", &datamodels.CategoryRel{Cid: cid, Aid: aid}, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_SelectManyByAid(t *testing.T) {
 	var (
-		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		expected = uint(2)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAidAndCid(in1, in2)
-	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAidAndCid(%d, %d) = %t; expected %t", in1, in2, actual, expected)
+	mysql.NewMysql().Exec("truncate table category_rel")
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.SelectManyByAid(aid)
+	if actual[0].Aid != expected {
+		t.Errorf("RCategoryRel.SelectManyByAid(%d) = %v; expected aid=%d", cid, actual, expected)
 	}
 }
 
 func Test_categoryRelRepository_SelectManyByCid(t *testing.T) {
 	var (
-		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
+		expected = uint(1)
+		cid      = uint(1)
+		aid      = uint(2)
 	)
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAidAndCid(in1, in2)
-	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAidAndCid(%d, %d) = %t; expected %t", in1, in2, actual, expected)
-	}
-}
-
-func Test_categoryRelRepository_Update(t *testing.T) {
-	var (
-		expected = true
-		in1      = uint(1)
-		in2      = uint(1)
-	)
-	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: in1, Aid: in2})
-	actual := RCategoryRel.DeleteByAidAndCid(in1, in2)
-	if actual != expected {
-		t.Errorf("RCategoryRel.DeleteByAidAndCid(%d, %d) = %t; expected %t", in1, in2, actual, expected)
+	mysql.NewMysql().Exec("truncate table category_rel")
+	RCategoryRel.Insert(&datamodels.CategoryRel{Cid: cid, Aid: aid})
+	actual := RCategoryRel.SelectManyByCid(cid)
+	if actual[0].Cid != expected {
+		t.Errorf("RCategoryRel.SelectManyByCid(%d) = %v; expected cid=%d", cid, actual, expected)
 	}
 }
---
+++ src/backend/repositories/category_repository_test.go
@@ -0,0 +1,26 @@
+package repositories
+
+import (
+	"testing"
+)
+
+func Test_categoryRepository_Delete(t *testing.T) {
+}
+
+func Test_categoryRepository_ExistName(t *testing.T) {
+}
+
+func Test_categoryRepository_Insert(t *testing.T) {
+}
+
+func Test_categoryRepository_Select(t *testing.T) {
+}
+
+func Test_categoryRepository_SelectMany(t *testing.T) {
+}
+
+func Test_categoryRepository_SelectManyByType(t *testing.T) {
+}
+
+func Test_categoryRepository_Update(t *testing.T) {
+}
